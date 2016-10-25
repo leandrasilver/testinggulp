4d0a52859102d4a76c85282389df5745517b3c47
Leandra <leandrasilver@gmail.com>
2016-10-25 12:48:14 -0400

    First commit gulp

---
+++ dev/scripts/main.js
@@ -0,0 +1,6 @@
+const ohHey = "Hello World";
+
+const numbers= [1,2,3];
+const maxNum = math.max(...numbers);
+
+console.log(ohHey);
---
+++ public/scripts/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var _math;
+
+var ohHey = "Hello World";
+
+var numbers = [1, 2, 3];
+var maxNum = (_math = math).max.apply(_math, numbers);
+
+console.log(ohHey);
